0f770d790bfe206664cdaca97f3f8967319db85b
azamie <ishii.kami2001@gmail.com>
2024-01-27 10:16:17 +0700

    Setup none-ls for nvim

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -13,25 +13,25 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- Detect tabstop and shiftwidth automatically
-	'tpope/vim-sleuth',
+  'tpope/vim-sleuth',
 
-	-- "gc" to comment visual regions/lines
-	{ 'numToStr/Comment.nvim', opts = {} },
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
 
-	{ import = 'plugins' },
+  { import = 'plugins' },
 })
 
 local modules = {
-	"config.autocmds",
-	"config.options",
-	"config.keymaps",
-	"config.custom",
+  "config.autocmds",
+  "config.options",
+  "config.keymaps",
+  "config.custom",
 }
 
 for _, mod in ipairs(modules) do
-	local ok, err = pcall(require, mod)
-	-- config.custom may be empty. It's a optional module
-	if not ok and not mod == "config/custom" then
-		error(("Error loading %s...\n\n%s"):format(mod, err))
-	end
+  local ok, err = pcall(require, mod)
+  -- config.custom may be empty. It's a optional module
+  if not ok and not mod == "config/custom" then
+	  error(("Error loading %s...\n\n%s"):format(mod, err))
+  end
 end
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,12 +2,17 @@ return {
   {
  "nvim-lualine/lualine.nvim",
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    "nvim-tree/nvim-web-devicons"
   },
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
-        theme = 'dracula'
+        theme = "dracula",
+        component_separators = "|",
+        section_separators = "",
+      },
+      sections = {
+        lualine_c = {{'filename', path = 1 }}
       }
     })
   end
